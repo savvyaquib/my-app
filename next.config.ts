6d01152520401e593405768852a186b4877d9de3
Mohammad Aquib <itsmohammadaquib@gmail.com>
2026-07-09 13:19:05 +0530

    Revert "Api routes"

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  cacheComponents: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com"
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com"
-      }
-    ]
-  }
 };
 
 export default nextConfig;
